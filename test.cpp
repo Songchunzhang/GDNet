2b665595b11d35835670cc78c24b17ec00e07687
songchunzhang <1241400738@qq.com>
2021-09-08 13:03:15 +0800

    deal with bug

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void shangwang();
+void duanshipin();
+void yuefan();
+
+int main()
+{
+    shangwang();
+    duanshipin();
+    // yuefan();
+    return 0;
+}
